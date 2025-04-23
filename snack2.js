3164079c43371edc24320d4a2785dae6e8fdede2
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-04-23 11:23:38 +0200

    snack2

---
+++ snack2.js
@@ -0,0 +1,3 @@
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+const quadrato = (n) => n * n;
+console.log("Quadrato di 6:", quadrato(6));
